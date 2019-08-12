5ce83132f3221a51c0f2c7bdf57d14e6052b6b92
Florian Chevallier <florian.chevallier@in-tact.fr>
2019-08-12 23:39:17 +0200

    f

--- web-push-back/send.js
+++ web-push-back/send.js
@@ -27,6 +27,10 @@ function askForReservation(moment, date, nbPerson) {
   const url = `${baseUrl}booking%5Bdate%5D=${date}&booking%5BpaxCount%5D=${nbPerson}&booking%5Bmoment%5D=${moment}&booking%5Bevent%5D%5BeventId%5D=0&booking%5Bevent%5D%5BadultCount%5D=0&booking%5Bevent%5D%5BpayingChildCount%5D=0&booking%5Bevent%5D%5BfreeChildCount%5D=0&booking%5Bevent%5D%5BtotalChange%5D=0&booking%5Bevent%5D%5BtotalCreditCard%5D=0&booking%5Bevent%5D%5BtotalHolidayVoucher%5D=0&booking%5Bevent%5D%5BtotalRestaurantVoucher%5D=0`;
 
   request.get(url, (error, response, body) => {
+
+    if (error) {
+      console.log("error", error);
+    }
     const res = JSON.parse(body);
 
     const { roomspertime } = res;
