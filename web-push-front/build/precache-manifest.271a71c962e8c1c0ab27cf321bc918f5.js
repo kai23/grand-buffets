152676d9fcf5abc58c03e410a5e0af45458b77eb
Florian Chevallier <florian.chevallier@in-tact.fr>
2019-08-12 23:16:57 +0200

    Ajout de la build

---
+++ web-push-front/build/precache-manifest.271a71c962e8c1c0ab27cf321bc918f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c099020ea6b8311e26bff13449615f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "406592afd05a9d0d6339",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "51ab954870c883f493aa",
+    "url": "/static/js/2.3447c23e.chunk.js"
+  },
+  {
+    "revision": "406592afd05a9d0d6339",
+    "url": "/static/js/main.f00cbbb8.chunk.js"
+  },
+  {
+    "revision": "aca6b47c1c1c137ca4db",
+    "url": "/static/js/runtime~main.c24ec1dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
