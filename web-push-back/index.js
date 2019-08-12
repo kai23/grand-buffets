e67db6c47c9cc8023c506a5a0e40d9fd716c90af
Florian Chevallier <florian.chevallier@in-tact.fr>
2019-08-12 23:31:05 +0200

    fix

--- web-push-back/index.js
+++ web-push-back/index.js
@@ -19,7 +19,7 @@ dotenv.config()
 app.use(cors())
 app.use(bodyParser.json())
 
-webpush.setVapidDetails(process.env.WEB_PUSH_CONTACT, process.env.PUBLIC_VAPID_KEY, process.env.PRIVATE_VAPID_KEY)
+webpush.setVapidDetails("mailto: florian.chevallier03@gmail.com", 'BA13aNs9a9ALPCh2GARkwJ5JJ-fXdvaeo06Q7Du9TsxLvXZT3D19au-onAdpn61340Ey1F3aPsuVTAHEGaxeSZo', 'N70q2-pQYU0qUcXSlVKOsOJHcerIE_xtMlgQZEqD5Nw')
 
 app.post('/notifications/subscribe', (req, res) => {
   const subscription = req.body
